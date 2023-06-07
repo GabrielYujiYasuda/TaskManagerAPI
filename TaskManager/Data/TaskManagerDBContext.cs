5ed9426616343cdb7536438fffff7dde56bc3474
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-06-07 10:13:43 -0600

    Initial commit

---
+++ TaskManager/Data/TaskManagerDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
+
+namespace TaskManager.Data
+{
+    public class TaskManagerDBContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+
+        //Constructor
+        public TaskManagerDBContext(DbContextOptions<TaskManagerDBContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
