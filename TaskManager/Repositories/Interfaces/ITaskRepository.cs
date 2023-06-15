c555a5d5a79661c3dc46ae563e4e8f4dcf04fff2
Gabriel Yuji Yasuda <gabrielyyasuda@gmail.com>
2023-06-15 14:33:10 -0900

    Task settings

---
+++ TaskManager/Repositories/Interfaces/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskManager.Models;
+
+namespace TaskManager.Repositories.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<List<TaskModel>> GetAllTasks();
+        Task<TaskModel> GetTaskById(int id);
+        Task<TaskModel> AddTask(TaskModel taskModel);
+        Task<TaskModel> UpdateTask(TaskModel taskModel, int id);
+        Task<bool> DeleteTask(int id);
+
+    }
+}
